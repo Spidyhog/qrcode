c8ea64d74df3b62139f4b125a8ea60341dad5a59
Priyanshu Mittal <priyanshumitttal9999@gmail.com>
2021-06-13 20:12:55 +0530

    App

--- routes/qr.js
+++ routes/qr.js
@@ -6,4 +6,10 @@ const router = express.Router();
 
 router.post("/read_qr", qrReader.readQr);
 
+router.get("/test", (res) => {
+  res.status(200).json({
+    message: "TEST",
+  });
+});
+
 module.exports = router;
